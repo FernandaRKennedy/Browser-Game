76c58cd49e3dd6f286c3a191786f3724bb3a0cf9
Fernanda Reis Kennedy <fernandarkennedy@Fernandas-MBP.lan>
2022-04-18 21:05:38 -0400

    clouds animation

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-
+//defining
 const character = document.getElementById('character');
 const cupcake = document.getElementById('cupcake');
 const score = document.getElementById('score');
 
-
+//function jump
 function jump() {
   character.classList.add('jump-animation');
   setTimeout(() => {
     character.classList.remove('jump-animation');
    }, 500);
 }
-
+//adding key function for the jump
 document.addEventListener('keypress', () => {
   if (!character.classList.contains('jump-animation')) {
     jump();
   }
 }) 
 
-
+//seting parameters for the collision
 setInterval(() => {
   const characterTop = parseInt(window.getComputedStyle(character)
     .getPropertyValue('top'));
@@ -31,66 +31,13 @@ setInterval(() => {
     cupcake.style.display = '';
   }
 
-  if (cupcakeLeft < 50 && cupcakeLeft > 0 && characterTop > 150) {
-    alert("You got a score of: " + score.innerText +
-      "\n\nPlay again?");
-    location.reload();
+//colision detection
+  if (cupcakeLeft < 100 && cupcakeLeft > 0 && characterTop >= 620) {
+    console.log(characterTop)
+    console.log(cupcakeLeft)
+    //alert window
+    // alert("Your score is: " + score.innerText +
+    //   "\n\nPlay again?");
+    // location.reload();
   }
 }, 50);
-
-
-
-
-
-
-
-
-
-
-
-
-
-// old code //
-
-
-
-
-// const character = document.getElementById("character");
-// const cupcake = document.getElementById("cupcake");
-// const score= documet.getElementById("score");
-// function jump(){
-// 	character.classList.add("jump-animation");
-// 	setTimeout=>
-// 	character.classList.remove("jump-animation"), 500);
-
-// }
-//     // if(character.classList != "animate"){
-//     // character.classList.add("animate");
-//     // }
-//     // setTimeout(function(){
-//     //     character.classList.remove("animate");
-//     // },500);
-//     // console.log(character)
-
-  
-// }
-// var checkDead = setInterval(function() {
-//     const characterRect= character.getBoundingClientRect()
-//     const cupcakeRect= cupcake.getBoundingClientRect()
-//     console.log(characterRect.bottom)
-//     console.log(cupcakeRect.top)
-//       if(characterRect.bottom >=cupcakeRect.top && characterRect.right >= cupcakeRect.left){}
-
-//         cupcake.style.animation = "none";
-//         cupcake.style.display = "none";
-//         alert("Game Over. score:");  
-//         console.log(alert)// found something to cancel alert  
-//         cupcake.style.animation = "cupcake 1.5s infinite linear";
-//     }
-
-//     else{
-//         counter ++; 
-// 				document.getElementById("soreSpan");
-//         // document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
-//     }
-// }, 15);
