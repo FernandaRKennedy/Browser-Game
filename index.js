cbbbbffebb7d155bc08e0fc860f8c1b70f3d274c
Fernanda Reis Kennedy <fernandareisdp@gmail.com>
2023-04-13 20:56:35 -0400

    GRADABLE

--- index.js
+++ index.js
@@ -89,90 +89,9 @@ function handlejump(){
 
       }
 }  
-//audio
-// const sfx = {
-//   jump1 : new Howl({
-//     src: [
-//       'audio/jump.wav'
-//     ]
-  
-// }),
-// die : new Howl({
-//   src:[
-//     'audio/die.wav'
-//   ]
-// })
-// }
 
 //calling the start and
 document.addEventListener('keypress', (e) =>{
   start() 
 
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//old code
-// //adding key function for the jump
-// // const jump-animation = document.createElement("assets/jumping.gif");  
-// document.addEventListener('keypress', () => {
-//   if (!character.classList.contains('jump-animation')) {
-//     jump();
-//   }
-// }) 
-
-// //seting parameters for the collision
-// setInterval(() => {
-//   const characterTop = parseInt(window.getComputedStyle(character)
-//     .getPropertyValue('top'));
-//   const cupcakeLeft = parseInt(window.getComputedStyle(cupcake)
-//     .getPropertyValue('left'));
-//   score.innerText++;
-//   if (cupcakeLeft < 0) {
-//     cupcake.style.display = 'none';
-//   } else {
-//     cupcake.style.display = '';
-// }
-
-// // function showGameOver() {
- 
-// //   ctx.font = `${fontSize}px Verdana`;
-// //   ctx.fillStyle = "grey";
-// //   ctx.fillText("GAME OVER", cupcakeLeft, characterTop);
-// // }
-
-
-// // debugger;
-// //colision detection
-
-//   if (cupcakeLeft < 320 && cupcakeLeft > 0 && characterTop >= 520  ) {
-//     console.log(characterTop)
-//     console.log(cupcakeLeft)
-  
-//     // alert("Your score is: " + score.innerText +
-//     //   "\n\nPlay again?");
-//     // location.reload();
-  
-//   }
-// }, 50);
-
-// // function drawScore() {
-// //   ctx.font = "16px Arial";
-// //   ctx.fillStyle = "#0095DD";
-// //   ctx.fillText(`Score: ${score}`, 8, 20);
-// //   location.reload();
-// // }
-
-
-
